83c3832e2a8072f58bb8add8210a49fc6d373025
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-11-12 10:48:02 +0600

    cout multiple value

--- module-1/first.cpp
+++ module-1/first.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-    cout<<"hello world";
+    cout<<"hello world" << endl;
+
+    int a = 10, b = 7;
+    cout<<"My favorite number is "<< b << " out of " << a;
     return 0;
 }
