4e0099033b38513f585658c049de591e8eeab476
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-11-12 14:19:50 +0600

    ternary operator in c++

---
+++ module-1/ternary.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    x == 7 ? cout<<"This is a lucky number" : cout<<"This is not a lucky number";
+    return 0;
+}
