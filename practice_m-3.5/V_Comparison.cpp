3597908d058a8cc73ebd23bb6e7533061ccf3050
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-11-16 16:40:27 +0600

    solved codeforces problem V_comparision

---
+++ practice_m-3.5/V_Comparison.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int a;
+    cin>>a;
+    char s;
+    cin>>s;
+    int b;
+    cin>>b;
+    if( s == '>'){
+        if (a > b)
+        {
+            cout<<"Right";
+        }
+        else
+        {
+            cout<<"Wrong";
+        }       
+    }
+    else if( s == '<'){
+        if (a < b)
+        {
+            cout<<"Right";
+        }
+        else
+        {
+            cout<<"Wrong";
+        }       
+    }
+     else if( s == '='){
+        if (a == b)
+        {
+            cout<<"Right";
+        }
+        else
+        {
+            cout<<"Wrong";
+        }       
+    }
+    return 0;
+}
