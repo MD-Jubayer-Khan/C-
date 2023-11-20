d759ca59d5aef1a591a41b9b66a576439b5649b7
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-11-20 14:28:43 +0600

    string in c++

---
+++ module-5/string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    string s1 = "hello";
+    string s2 = "Hello";
+
+    if (s1 == s2)
+    {
+        cout << "Both are same";
+    }
+    else
+    {
+        cout << "They are not same";
+    }
+    
+    
+    return 0;
+}
