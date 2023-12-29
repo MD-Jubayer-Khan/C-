5c399c0eacd31ca10238aa5ed68bbe987328feaf
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-12-29 21:50:32 +0600

    solved problem get eliminated from phitron montly programming contest

---
+++ Monkey_and_Bamboo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int H, A, B;
+        cin >> H >> A >> B;
+        int difAB = A - B;
+    }
+    
+    return 0;
+}
