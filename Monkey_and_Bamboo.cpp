fde1f9a7d757d8d11e41d639b8d02af80fe52c3f
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-12-30 10:44:48 +0600

    phitron montly programming contest problem solution

--- Monkey_and_Bamboo.cpp
+++ Monkey_and_Bamboo.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
-
+#define ll long long
 using namespace std;
-
-int main () {
-    int t;
+int main()
+{
+    ll t;
     cin >> t;
     while (t--)
     {
-        int H, A, B;
-        cin >> H >> A >> B;
-        int difAB = A - B;
+        ll h, a, b;
+        cin >> h >> a >> b;
+        if (h <= a)
+        {
+            cout << 1 << endl;
+        }
+        else if (a > b)
+        {
+            ll ans = ceil((h - a) * 1.0 / (a - b) * 1.0);
+            cout << ans * 2 + 1 << endl;
+        }
+        else
+        {
+            cout << "Impossible" << endl;
+        }
     }
-    
     return 0;
 }
